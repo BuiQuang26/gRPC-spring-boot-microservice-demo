93e61a300e5d3a6e91e75d904dec7d706e409c6c
quangbs <quangbs@selex.vn>
2025-03-11 12:34:15 +0700

    first commit

---
+++ bookService/src/test/java/vn/buiquang26/bookService/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.buiquang26.bookService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
