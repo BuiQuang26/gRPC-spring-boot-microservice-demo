7fae052213647f54090f48fa8aae38d3dc0ed3ec
quangbs <quangbs@selex.vn>
2025-03-11 15:23:18 +0700

    config discovery service

--- bookService/src/main/java/vn/buiquang26/bookService/BookServiceApplication.java
+++ bookService/src/main/java/vn/buiquang26/bookService/BookServiceApplication.java
@@ -2,7 +2,9 @@ package vn.buiquang26.bookService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BookServiceApplication {
 
--- userService/src/main/java/vn/buiquang26/userService/UserServiceApplication.java
+++ userService/src/main/java/vn/buiquang26/userService/UserServiceApplication.java
@@ -2,7 +2,9 @@ package vn.buiquang26.userService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserServiceApplication {
 
